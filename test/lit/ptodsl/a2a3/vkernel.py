daaef313b6ee50bfc7592e18b87cb280c717ae5a
Giacomo Castiglioni <giacomo.castiglioni@huawei.com>
2026-05-21 13:45:08 +0200

    first minimal test

---
+++ test/lit/ptodsl/a2a3/vkernel.py
@@ -0,0 +1,28 @@
+# RUN: %PYTHON %s | FileCheck %s
+from ptodsl import pto
+
+# RUN: %PYTHON %s | mlir-opt -split-input-file | FileCheck
+
+# TODO: Move to a test utility class
+def print_module(handle):
+    # Get the MLIR module from the handle
+    mod = handle.mlir_module()
+    # Get the func name from the module
+    func = mod.body.operations[0]
+    print("// -----")
+    print("// TEST_FUNCTION:", func.name.value)
+    print(mod.operation.get_asm())
+    return handle
+
+# CHECK-LABEL: TEST_FUNCTION: miminal_kernel
+# CHECK: module attributes {pto.kernel_kind = #pto.kddernel_kind<vector>, pto.target_arch = "a2a3"} {
+@print_module
+@pto.jit(
+    name="miminal_kernel",
+    kernel_kind="vector",
+    target="a2a3",
+    func_attr="pto.aicore",
+)
+def miminal_kernel():
+    c0   = pto.const(0)
+
